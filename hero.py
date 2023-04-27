f7dc144ce38d8b71db7ed15259115bc44db91588
MAGANER <maganer.krik@mail.ru>
2023-04-27 20:13:06 +0300

    class, that represents hero

---
+++ hero.py
@@ -0,0 +1,24 @@
+class Hero(object):
+    def __init__(self):
+        self.__x = 30
+        self.__y = 19
+        self.shooting_time_counter = 0
+    def get_x(self):
+        return self.__x
+    def get_y(self):
+        return self.__y
+
+
+    def tic(self):
+        self.shooting_time_counter = self.shooting_time_counter + 1
+    def can_shoot(self):
+        if self.shooting_time_counter > 5:
+            self.shooting_time_counter = 0
+            return True
+        return False
+    def move_left(self):
+        self.__x = self.__x -1
+        return self.__x
+    def move_right(self):
+        self.__x = self.__x + 1
+        return self.__x
