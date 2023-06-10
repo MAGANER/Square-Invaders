6f41b3664bbfd9b3e997e1cd33fd5595a6611bce
user <user@A320WS1.ispo.spbstu.ru>
2023-06-10 13:29:59 +0300

    added super attack

--- hero.py
+++ hero.py
@@ -18,6 +18,11 @@ class Hero(object):
             self.shooting_time_counter = 0
             return True
         return False
+    def can_shoot_super(self):
+        if self.score >= 100:
+            self.score -= 100
+            return True
+        return False
     def move_left(self):
         self.__x = self.__x -1
         return self.__x
