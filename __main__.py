1867bc3188fe89d9cd64be7c1228cf524c11d0a7
MAGANER <maganer.krik@mail.ru>
2023-10-09 07:36:19 +0300

    if it's not unix, then break attempt to run terminal mode

--- __main__.py
+++ __main__.py
@@ -1,7 +1,11 @@
+import os
 import sys
 
 if __name__ == "__main__":
     if len(sys.argv) == 2 and sys.argv[1] == "classic":
+        if os.name == "nt":
+            print("Sorry! Terminal Square Invaders can not be run on Windows! But you can play graphical mode!")
+            exit(0) 
         import terminal_main
         terminal_main.run_game()
     else:
